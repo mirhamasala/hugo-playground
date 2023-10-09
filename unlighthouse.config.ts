a19e4f45b23d8ed0c45481bd3d17926f370d0f91
Mirha Masala <mirha@hey.com>
2023-10-09 12:59:55 +0100

    Add Unlighthouse reports

---
+++ unlighthouse.config.ts
@@ -0,0 +1,10 @@
+export default {
+  site: "https://www.mirhamasala.com",
+  outputPath: "static/unlighthouse",
+  routerPrefix: "/unlighthouse",
+  cache: true,
+  debug: true,
+  scanner: {
+    device: "desktop",
+  },
+};
